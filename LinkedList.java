b1e1a8f7f564ad63fab9d3f0b52f1ec71b7cd138
Aarushi Jain <aarushi1612002@gmail.com>
2023-06-13 15:45:26 +0530

    Create LinkedList.java

---
+++ LinkedList.java
@@ -0,0 +1,13 @@
+import org.w3c.dom.Node;
+
+//implementation of linked list
+class LinkedList{
+    Node head;
+
+    class Node{
+        int data;
+        Node next;
+         Node(int d) 
+         { data = d; }
+    }
+}
