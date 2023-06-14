d58a3b8d35260b810e04b4faf9eae65cc56d858b
Aarushi Jain <aarushi1612002@gmail.com>
2023-06-14 14:06:47 +0530

    .

--- q1pla.java
+++ q1pla.java
@@ -12,16 +12,18 @@ public class q1pla {
 
     public static void twoSum(int[] nums, int target) {
         for (int i = 0; i < nums.length; i++) {
-            int sum = 0;
-
-            if (nums[i] <= target) {
-                int[] sumNums = {target - nums[i]};
-
-                for (int j = 0; j < sumNums.length; j++) {
-                    sum += sumNums[j];
-                    System.out.println(sum);
+            for (int j=i+1; j<nums.length; j++)
+            {
+                if(nums[i] + nums[j] == target)
+                {
+                    System.out.printf("pair found (%d, %d)", nums[i], nums[j]);
+                    return;
                 }
             }
+            
         }
+                 
     }
 }
+        
+
