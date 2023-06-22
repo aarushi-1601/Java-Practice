c3e5a3e4b96c149fa3af40b10c2799b2bb673a5b
Aarushi Jain <aarushi1612002@gmail.com>
2023-06-22 19:27:59 +0530

    .

---
+++ q5pla.java
@@ -0,0 +1,17 @@
+// inserion of element in beginning and ending of linked list
+
+import java.util.LinkedList;
+public class q5pla{
+    public static void main(String args[]){
+        LinkedList <String> l_list = new
+        LinkedList<String>();
+        l_list.add("Green");
+        l_list.add("Blue");
+        l_list.add("Yellow");
+        System.out.println("Original Linked list is:"+l_list);
+
+        l_list.addFirst("Black");
+        l_list.addLast("Purple");
+        System.out.println("Final Linked lis is:"+l_list);
+    }
+}
